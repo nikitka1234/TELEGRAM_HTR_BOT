48d0002542188ab2fed632418e6674d15fb26a3c
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2022-07-28 14:24:02 +0500

    htr_bot

--- photo_processing/start_processing.py
+++ photo_processing/start_processing.py
@@ -10,7 +10,7 @@ async def start_processing(file_id: int):
     url = f'http://176.53.160.122:8000/api/predict?file_id={file_id}'
 
     data = {
-        "file_id": bytes(file_id),
+        "file_id": bytes(file_id)
     }
 
     async with aiohttp.ClientSession() as session:
