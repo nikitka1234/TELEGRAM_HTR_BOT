f99587073728fc1a8f6d412ecc5e41df469c3664
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2022-07-28 14:18:48 +0500

    htr_bot

--- photo_processing/start_processing.py
+++ photo_processing/start_processing.py
@@ -7,7 +7,7 @@ headers = {
 
 
 async def start_processing(file_id: int):
-    url = f'http://176.53.160.122:8000/api/get?file_id{file_id}'
+    url = f'http://176.53.160.122:8000/api/get?file_id={file_id}'
 
     data = {
         "file_id": bytes(file_id),
