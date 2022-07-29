867aab3927f8904b9328bf89bf012b7fb52355cd
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2022-07-29 14:44:21 +0500

    htr_bot

--- photo_processing/send_photo.py
+++ photo_processing/send_photo.py
@@ -7,13 +7,13 @@ headers = {
 
 
 async def post_request(file: str, file_id: int, tag: str, photo_number: int, caption: str = 'null'):
-    url = f'http://176.53.160.122:8000/api/upload?file_id={file_id + photo_number}&tag={tag}'
+    url = f'http://176.53.160.122:8000/api/upload?file_id={file_id + photo_number}&tag={tag}&text={caption.lower()}'
 
     data = {
         "file_id": bytes(file_id + photo_number),
         "tag": tag,
         "file": open(file, 'rb'),
-        "text": str(caption).lower()
+        "text": caption.lower()
     }
 
     async with aiohttp.ClientSession() as session:
